0b1e80b14de243444451bc403822d9eed1a0651c
johnh31 <jona9331@hotmail.com>
2020-10-29 20:28:23 -0600

    data

--- lib/src/bloc/provider.dart
+++ lib/src/bloc/provider.dart
@@ -3,22 +3,18 @@ import 'package:formulario/src/bloc/login_bloc.dart';
 export 'package:formulario/src/bloc/login_bloc.dart';
 
 class Provider extends InheritedWidget {
-  //static Provider _instancia;
+  static Provider _instancia;
 
-  //factory Provider({Key key, Widget child}) {
-  //if (_instancia == null) {
-  //_instancia = new Provider._internal(key: key, child: child);
-  //}
+  factory Provider({Key key, Widget child}) {
+    if (_instancia == null) {
+      _instancia = new Provider._internal(key: key, child: child);
+    }
+    return _instancia;
+  }
 
-  //return _instancia;
-  //}
-//._internal
-  Provider({Key key, Widget child}) : super(key: key, child: child);
+  Provider._internal({Key key, Widget child}) : super(key: key, child: child);
 
   final loginBloc = LoginBloc();
-
-  // Provider({ Key key, Widget child })
-  //   : super(key: key, child: child );
 
   @override
   bool updateShouldNotify(InheritedWidget oldWidget) => true;
